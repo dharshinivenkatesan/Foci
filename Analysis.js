005736a883c5f35a554cf547f4c955e0e3cfe4bb
Dharshini Venkatesan <106605192+dharshinivenkatesan@users.noreply.github.com>
2023-10-28 16:34:08 +0530

    Create Analysis.js

---
+++ Analysis.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Analysis = () => {
+  return (
+    <div className="analysis">
+      <h2>Analysis Section</h2>
+      <div className="analysis-options">
+        <h3>Analysis Options</h3>
+        <button className="weekly">Weekly</button>
+        <button className="monthly">Monthly</button>
+      </div>
+      <div className="bar-chart">
+        <h3>Study Analysis</h3>
+        <p>Placeholder content for the study analysis bar chart.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Analysis;
