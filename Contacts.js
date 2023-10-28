55e095606ff0b9dcef16db38cdfd0531c8c15894
Dharshini Venkatesan <106605192+dharshinivenkatesan@users.noreply.github.com>
2023-10-28 18:45:46 +0530

    Create Contacts.js

---
+++ Contacts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <div className="contacts">
+      <h2>Contacts Section</h2>
+      <ul className="contact-list">
+        <li>Contact 1</li>
+        <li>Contact 2</li>
+        <li>Contact 3</li>
+        {/* Add more contacts as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default Contacts;
